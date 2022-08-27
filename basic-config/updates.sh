2dd6ba4c37ede37bc4bb9d9db48a2213e779e257
Rishi <rxerium@pm.me>
2022-08-27 19:36:03 +0100

    new-scripts

--- basic-config/updates.sh
+++ basic-config/updates.sh
@@ -1,8 +1,13 @@
 # Updates and upgrades the packages on the Linux machine
 
 #!/bin/bash
+echo "Script starting..."
+sleep 1
+echo "Updating the system..."
 sudo apt-get update
 sleep 2
+echo "Upgrading the system..."
 sudo apt-get upgrade -y
-sleep 2 
+echo "Upgrade complete, rebooting in 10 seconds..."
+sleep 10 
 sudo reboot
